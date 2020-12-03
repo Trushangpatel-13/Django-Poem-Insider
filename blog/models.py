27d0cc0567ebece2896be66114d53eef65d6f37a
Trushangpatel-13 <ptrushang@gmail.com>
2020-12-03 18:26:20 +0530

    .

--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,7 @@ class Category(models.Model):
     name = models.CharField(max_length=20)
     def __str__(self):
         return self.name
-    def get_absolute_url (self):
-        return reverse('article-detail',args=(str(self.id)))
-        #return reverse('home')
+
 
 
 class Post(models.Model):
