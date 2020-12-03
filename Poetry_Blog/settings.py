44571e0d185c3893ef678525d2d209dd9d5cd715
Trushangpatel-13 <ptrushang@gmail.com>
2020-12-03 10:11:30 +0530

    DB info changed

--- Poetry_Blog/settings.py
+++ Poetry_Blog/settings.py
@@ -107,7 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -126,6 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
