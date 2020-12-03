8b21e7fd1947e63d6f95510a0d06998faaefdfcd
Trushangpatel-13 <ptrushang@gmail.com>
2020-12-03 21:50:17 +0530

    .

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import Post,Category
 choices = Category.objects.all().values_list('name','name')
+choices_list = []
+for item in choices:
+    choices_list.append(item)
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
@@ -9,7 +12,7 @@ class PostForm(forms.ModelForm):
             'title':forms.TextInput(attrs={'class':'form-control','placeholder':'Title of the Blog'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control','placeholder':'Tag'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
-            'category':forms.Select(choices=choices,attrs={'class':'form-control'}),
+            'category':forms.Select(choices=choices_list,attrs={'class':'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control','placeholder':'Describe your beautiful creation'}),
 
         }
