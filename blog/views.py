13cf22bbd4a793a72afe695f35b58331853f237d
Trushangpatel-13 <ptrushang@gmail.com>
2020-12-03 18:12:06 +0530

    Auth, Categories added | slugified URL

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
-from .models import Post
+from .models import Post,Category
 from .forms import PostForm,UpdatePostForm
 from django.urls import reverse_lazy
 #def home(request):
@@ -11,9 +11,21 @@ class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     ordering = ['-pub_Date']
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView,self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_detail.html'
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView,self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
 class AddPostView(CreateView):
     model = Post
     form_class = PostForm
@@ -28,3 +40,7 @@ class DeletePostView(DeleteView):
     form_class = UpdatePostForm
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
+
+def CategoryView(requests,cats):
+    category_post = Post.objects.filter(category=cats.title().replace('-',' '))
+    return render(requests,'categories.html',{'cats':cats.title().replace('-',' '),'category_post':category_post})
