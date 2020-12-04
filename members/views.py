1537932ffe5effa27d3a96e0728129d728629fcf
Trushangpatel-13 <ptrushang@gmail.com>
2020-12-04 17:40:14 +0530

    Login File name changed

--- members/views.py
+++ members/views.py
@@ -7,4 +7,4 @@ from django.urls import reverse_lazy
 class UserRegisterView(generic.CreateView):
     form_class = UserCreationForm
     template_name = 'registration/registration.html'
-    success_url = reverse_lazy('Login')
+    success_url = reverse_lazy('login')
